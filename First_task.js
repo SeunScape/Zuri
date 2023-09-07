2ef8a97aeca5ee06adfb6ede2d255f9229a74172
RIX <69199129+SeunScape@users.noreply.github.com>
2023-09-07 02:10:18 +0100

    apiURL

--- First_task.js
+++ First_task.js
@@ -4,15 +4,35 @@ const app = express ();
 app.use(express.json());
 
 app.get("/user", (req, res) => {
-    const {slack_name, track} = req.query
-})
+    const {slack_name, track} = req.query;
+
 
 const todaysDate = new Date ();
 const currentDay = todaysDate.toLocaleDateString('en-US', { weekday: 'long' });
-const utcTime = currentDate.toISOString();
+const utcTime = todaysDate.toISOString();
 const githubFileUrl = "https://github.com/SeunScape/";
-const githubRepoUrl = ""
+const githubRepoUrl = "https://github.com/SeunScape/Zuri";
 
-app.listen(PORT, () => {
-    console.log("Server Listening on PORT:", port);
+if (!slack_name || !track) {
+    return res.status(400).json({
+      error: 'Include slack_name and track in your http query',
+    });
+  }
+
+res.json({
+    slack_name,
+    "current_day": currentDay,
+    "utc_time": utcTime,
+    track,
+    "github_file_url": githubFileUrl,
+    "github_repo_url": githubRepoUrl,
+    status_code: 200
+})
+
+})
+const port = process.env.PORT || 3000;
+const hostname = 'example.com';
+
+app.listen(port, () => {
+    console.log(`Server Listening on ${hostname}: ${port}`);
   });
